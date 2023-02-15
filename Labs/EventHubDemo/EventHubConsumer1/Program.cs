6bb3d22c060c370049e58503f6b9ca1cffdf4036
Sandeep Soni <sandeepsoni@deccansoft.com>
2023-02-15 19:32:51 +0530

    EventHub and EventGrid

---
+++ Labs/EventHubDemo/EventHubConsumer1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+class Program
+{
+    private const string ehubNamespaceConnectionString = "Endpoint=sb://dsdemo-eventhub.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=jeUBqrJ+B7dtI0CeRfoWKwGKaa7CLH/gb+AEhOl4y8o=";
+    private const string eventHubName = "demohub2";
+    private const string blobStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=dsdemostorage123;AccountKey=VG3p49BDiHlYn2szJuRHJXsW/RTYf2JmXAHTm+MwUWnNUH3vuSCqaWLkmFpyhMkHOxbotCIgvE8a+AStf9A47w==;EndpointSuffix=core.windows.net";
+    private const string blobContainerName = "ehcon2"; 
+
+    static async Task Main()
+    {
+        // Read from the default consumer group: $Default
+        string consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
+        // Create a blob container client that the event processor will use 
+        BlobContainerClient storageClient = new BlobContainerClient(blobStorageConnectionString, blobContainerName);
+        // Create an event processor client to process events in the event hub
+        EventProcessorClient processor = new EventProcessorClient(storageClient, consumerGroup, ehubNamespaceConnectionString, eventHubName);
+        
+        // Register handlers for processing events and handling errors
+        processor.ProcessEventAsync += ProcessEventHandler;
+        processor.ProcessErrorAsync += ProcessErrorHandler;
+
+        // Start the processing
+        await processor.StartProcessingAsync();
+        Console.WriteLine("Press Enter to stop...");
+        Console.ReadLine();
+        // Stop the processing
+        await processor.StopProcessingAsync();
+    }
+    static async Task ProcessEventHandler(ProcessEventArgs eventArgs)
+    {
+        // Write the body of the event to the console window
+        Console.WriteLine("\tRecevied event: {0} - {1}", Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()), eventArgs.Partition.PartitionId);
+        // Update checkpoint in the blob storage so that the app receives only new events the next time it's run
+        await eventArgs.UpdateCheckpointAsync(eventArgs.CancellationToken);
+    }
+    static Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
+    {
+        // Write details about the error to the console window
+        Console.WriteLine($"\tPartition '{ eventArgs.PartitionId}': an unhandled exception was encountered. This was not expected to happen.");
+        Console.WriteLine(eventArgs.Exception.Message);
+        return Task.CompletedTask;
+    }
+}
---
+++ Labs/EventHubDemo/EventHubProducer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+
+class Program
+{
+    private const string ehubNamespaceConnectionString = "Endpoint=sb://dsdemo-eventhub.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Knma+0+5EIWk+stT2xWwcv8rflSKhKKVW+AEhCVuFjE=";
+    private const string eventHubName = "demohub2";
+    static async Task Main()
+    {
+        // Create a producer client that you can use to send events to an event hub
+        await using (var producerClient = new EventHubProducerClient(ehubNamespaceConnectionString, eventHubName))
+        {
+            while (true)
+            {
+                // Create a batch of events 
+                using EventDataBatch eventBatch = await producerClient.CreateBatchAsync();
+                
+                // Add events to the batch. An event is a represented by a collection of bytes and metadata. 
+                string time = DateTime.Now.ToLongTimeString();
+                var ed = new EventData(Encoding.UTF8.GetBytes("First event at " + time));
+
+                eventBatch.TryAdd(ed);
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("Second event " + time)));
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("Third event " + time)));
+                
+                // Use the producer client to send the batch of events to the event hub
+                await producerClient.SendAsync(eventBatch);
+                Console.WriteLine("A batch of 3 events has been published at " + time + "...Press enter for next batch");
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
